55d92469ce597c2b978368c42f65fb6288489f29
城戸双汰朗 <Sooochan2001@gmail.com>
2024-05-25 14:15:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const DrawTypes = {
 	Triangle: 2
 };
 
-var imgNames = ["img/chara_ui","img/1CC.png","img/2DRD.png","img/3CC.png"];
+var imgNames = ["img/chara_ui.png","img/1CC.png","img/2DRD.png","img/3CC.png"];
 
 var particles = [];
 var indices = [];
